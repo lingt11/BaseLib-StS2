57b2639781d5f4350294f9b60f154b621725daaa
Alchyr <isithalchyr@gmail.com>
2026-03-16 22:52:14 -0700

    add more activation restrictions

--- Patches/Features/SelfApplyDebuffPatch.cs
+++ Patches/Features/SelfApplyDebuffPatch.cs
@@ -1,3 +1,4 @@
+using BaseLib.Abstracts;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
@@ -8,26 +9,27 @@ using MegaCrit.Sts2.Core.Models;
 namespace BaseLib.Patches.Features;
 
 /// <summary>
-/// If player apply debuff to self or ally before turn ends (not from curse cards like doubt),
+/// If player applies a debuff to self or ally before turn ends (not from curse cards like doubt),
 /// the debuff should tick it's duration at enemy's turn ends
 /// </summary>
 [HarmonyPatch(typeof(PowerCmd), nameof(PowerCmd.Apply), [typeof(PowerModel), typeof(Creature), typeof(decimal), typeof(Creature), typeof(CardModel), typeof(bool)])]
 public static class SelfApplyDebuffPatch
 {
     [HarmonyPostfix]
-    static void Postfix(ref Task __result, PowerModel power, Creature target)
+    static void Postfix(ref Task __result, PowerModel power, Creature target, Creature? applier, CardModel? cardSource)
     {
-        // Replace the original Task with our wrapped Task
-        __result = WrappedApplyTask(__result, power, target);
+        // Replace original Task with wrapped Task
+        __result = WrappedApplyTask(__result, power, target, applier, cardSource);
     }
 
-    static async Task WrappedApplyTask(Task originalTask, PowerModel power, Creature target)
+    static async Task WrappedApplyTask(Task originalTask, PowerModel power, Creature target, Creature? applier, CardModel? cardSource)
     {
-        // First, wait for all await methods in the original function (including Hook.AfterPowerAmountChanged, etc.) to complete execution.
         await originalTask;
 
         // At this point, all the logic in the original function has been executed, including the part that sets SkipNextDurationTick to true.
-        if (target.Side == CombatSide.Player && power.Type == PowerType.Debuff && power.Applier?.Side == CombatSide.Player)
+        if (target.CombatState?.CurrentSide == CombatSide.Player 
+            && target.Side == CombatSide.Player && power is { Type: PowerType.Debuff, Applier.Side: CombatSide.Player }
+            && (power.Applier?.Monster is ICustomModel || power.Applier?.Player?.Character is ICustomModel || cardSource is ICustomModel))
         {
             // Ensure player-applied debuffs on self/allies tick at current (enemy's) turn end
             power.SkipNextDurationTick = false;
